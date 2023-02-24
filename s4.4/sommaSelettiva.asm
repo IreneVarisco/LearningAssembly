0ac8fd586f0759f1e9484629eec89651e97c36fd
IreneVarisco <variscoirene@gmail.com>
2023-02-24 10:52:02 +0100

    es 4.4

--- s4.4/sommaSelettiva.asm
+++ s4.4/sommaSelettiva.asm
@@ -1,23 +1,23 @@
 #Si scriva un programma che:
 
-#* chieda all’utente di inserire un array di interi di dimensione arbitraria (si scelga una dimensione massima da non eccedere). 
+#* chieda allï¿½utente di inserire un array di interi di dimensione arbitraria (si scelga una dimensione massima da non eccedere). 
 #* invochi una procedura P 
 #* stampi il valore ritornato da P
 
-#La procedura P è definita come segue:
+#La procedura P ï¿½ definita come segue:
 
-#* Input: l’array inserito dall’utente e un parametro k
-#* se k=0 la procedura calcola la somma di tutti gli interi in posizione (indice nell’array) dispari
-#* se k=1 sommerà quelli in posizioni pari.
+#* Input: lï¿½array inserito dallï¿½utente e un parametro k
+#* se k=0 la procedura calcola la somma di tutti gli interi in posizione (indice nellï¿½array) dispari
+#* se k=1 sommerï¿½ quelli in posizioni pari.
 
-#Suggerimento: allocare l’array staticamente in memoria e passare alla procedura il base address (passaggio per indirizzo).
+#Suggerimento: allocare lï¿½array staticamente in memoria e passare alla procedura il base address (passaggio per indirizzo).
 
 .data
 	s: .asciiz "inserire lunghezza array"
 	s1: .asciiz "inserire elementi array"
 	.align 2
 	A: .space 10
-
+ 
 .text
 
 li $v0 4
